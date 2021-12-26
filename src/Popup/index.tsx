278cca9e2c32cdd16c8dd432a02e0cbf1285df1e
ulcttku <ulcttku@gmail.com>
2021-12-26 10:33:44 +0900

    create popup

---
+++ src/Popup/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Popup: React.VFC = () => {
+    return (
+        <div style={{width: '200px'}}>
+            Hello Chrome Extensions
+        </div>
+    );
+}
+
+export default Popup;
